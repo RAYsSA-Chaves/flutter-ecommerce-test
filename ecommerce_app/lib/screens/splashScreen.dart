377b72534534c7a431521503c06a0e16d2d9132d
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2026-05-06 22:28:22 -0300

    splash

---
+++ ecommerce_app/lib/screens/splashScreen.dart
@@ -0,0 +1,101 @@
+import 'package:ecommerce_app/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3), 
+      () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // captura o tamanho da tela
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              'images/wave1.png',
+              width: size.width * 0.5, 
+            ),
+          ),
+
+          Positioned(
+            top: size.height * 0.02,
+            right: 0,
+            child: Image.asset(
+              'images/wave2.png',
+              width: size.width * 0.3, 
+            ),
+          ),
+
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0, // left e right = 0 faz a imagem querer esticar horizontalmente
+            child: SvgPicture.asset(
+              'images/wave3.svg',
+              fit: BoxFit.fitWidth, 
+            ),
+          ),
+
+          Positioned(
+            bottom: size.height * 0.2,
+            left: 0,
+            right: 0,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'images/splashMan.png',
+                height: size.height * 0.45, 
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: size.height * 0.2, 
+            left: 32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'images/logo.png',
+                  width: size.width * 0.35,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Encontre os melhores livros!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
