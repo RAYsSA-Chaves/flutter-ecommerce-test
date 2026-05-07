a2721e93744f0770f7bfb9a7095c5d7114e6caa3
RAYsSA-Chaves <rayssa.ccmelo@gmail.com>
2026-05-07 16:47:25 -0300

    splash

--- ecommerce_app/lib/screens/splashScreen.dart
+++ ecommerce_app/lib/screens/splashScreen.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 
 class SplashScreen extends StatefulWidget {
@@ -15,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(
-      Duration(seconds: 3), 
+      Duration(seconds: 2), 
       () {
         Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
       }
@@ -34,8 +35,8 @@ class _SplashScreenState extends State<SplashScreen> {
           Positioned(
             top: 0,
             left: 0,
-            child: Image.asset(
-              'images/wave1.png',
+            child: SvgPicture.asset(
+              'images/wave1.svg',
               width: size.width * 0.5, 
             ),
           ),
@@ -43,14 +44,14 @@ class _SplashScreenState extends State<SplashScreen> {
           Positioned(
             top: size.height * 0.02,
             right: 0,
-            child: Image.asset(
-              'images/wave2.png',
+            child: SvgPicture.asset(
+              'images/wave2.svg',
               width: size.width * 0.3, 
             ),
           ),
 
           Positioned(
-            bottom: 0,
+            bottom: -10,
             left: 0,
             right: 0, // left e right = 0 faz a imagem querer esticar horizontalmente
             child: SvgPicture.asset(
@@ -60,7 +61,7 @@ class _SplashScreenState extends State<SplashScreen> {
           ),
 
           Positioned(
-            bottom: size.height * 0.2,
+            bottom: size.height * 0.23,
             left: 0,
             right: 0,
             child: Align(
@@ -74,7 +75,22 @@ class _SplashScreenState extends State<SplashScreen> {
           ),
 
           Positioned(
-            top: size.height * 0.2, 
+            bottom: size.height * 0.19,
+            left: size.width * 0.4,
+            child: LinearPercentIndicator(
+              width: size.width * 0.2,
+              lineHeight: 8,
+              percent: 1.0,
+              animation: true,
+              animationDuration: 2000,
+              barRadius: Radius.circular(10),
+              progressColor: Colors.black,
+              backgroundColor: Colors.grey.shade300,
+            ),
+          ),
+
+          Positioned(
+            top: size.height * 0.18, 
             left: 32,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
